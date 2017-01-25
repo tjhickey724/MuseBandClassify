2693f465151dd46b7006d9c16b8e9e6ebd8d47ef
tim <tjhickey@brandeis.edu>
2017-01-25 16:26:41 -0500

    added some cluster analysis with sliding window

--- analysisDemo.m
+++ analysisDemo.m
@@ -5,13 +5,21 @@ all = all(:,2:21); % throw out the first parameter which is the time in seconds
 musePlot(all);  % look at all data, select some
 
 % classify 
-[idx,X,sumd,D] = kmeans(all,8);
+[idx,X,sumd,D] = kmeans(all,10);
 
 [C,D]=museClassifyAll(all,1,X);
 
 % plot
 hold off;
-musePlot(all);
-hold on; plot(C*0.2+3.55,'.b');hold off;
+subplot(3,1,1);
+musePlot(all);grid on; grid minor;
+subplot(3,1,2);
+plot(C*0.2+3.55,'.b');grid on; grid minor;
+subplot(3,1,3);
+zz = clusterWindow(C,600);
+plot(zz);
+legend(string(1:max(C)));
+grid on; grid minor;
+
 
 
