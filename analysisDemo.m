acfff5086f1ed78af4de19af14c34285dabaa2bf
tim <tjhickey@brandeis.edu>
2017-02-05 12:44:13 -0500

    added temporal line to PCA plots

--- analysisDemo.m
+++ analysisDemo.m
@@ -1,9 +1,9 @@
 % This produces five plots based on the number of clusters:
-numClusters = 24
+numClusters = 12;
 
-windowSize = 600
+windowSize = 600;
 
-figure
+f1 = figure(1);
 
 % The top plot is the five bands for each of the 4 electrodes
 
@@ -110,27 +110,156 @@ grid on; grid minor;
 xticks([0:3000:12000])
 % look into analyzing data using pca(all) to see which
 % vectors have the most importance, and cluster on those??
-clusters = [1:numClusters]
-mathClusters = clusters(vv(1,:)==1)
-openClusters = clusters(vv(2,:)==1)
-readClusters = clusters(vv(3,:)==1)
-closedClusters = clusters(vv(4,:)==1)
-title('Plot D: 1 minute smoothing of k-means classification')
+clusters = [1:numClusters];
+mathClusters = clusters(vv(1,:)==1);
+openClusters = clusters(vv(2,:)==1);
+readClusters = clusters(vv(3,:)==1);
+closedClusters = clusters(vv(4,:)==1);
+title('Plot D: 1 minute smoothing of k-means classification');
 
 
 
 % Next we plot the Markov model of all cluster nodes
 % if control passes from one to the other more than Cutoff times
 % we draw an edge between them
-Cutoff=5
+% Cutoff=5
 B=zeros(numClusters,numClusters);
 for i=[1:length(C)-1]
   B(C(i),C(i+1)) = B(C(i),C(i+1))+1;
 end
-figure
+
+
+f2 = figure(2);
 coords= [aa(:,1)-0.7*aa(:,3),aa(:,2)-0.7*aa(:,3)];
 
-C = B-min(B,Cutoff)+Cutoff
-C(C==Cutoff) = 0
-gplot(C,coords)
+Cutoff = 0;
+drawMM(aa,B,Cutoff);
+%{
+BB = B-min(B,Cutoff)+Cutoff
+BB(BB==Cutoff) = 0
+gplot(BB,coords)
 title('Markov Model of Cluster Points')
+%}
+
+%{
+% create a symmetric version of the digraph
+B2 = B+B';
+BB = B2-min(B2,Cutoff)+Cutoff;
+BB(BB==Cutoff) = 0;
+BB = BB - BB.*eye(size(BB)); % remove diagonal
+% use it to create a graph object
+G = graph(BB);
+
+% scale the line widths
+LWidths = 5*G.Edges.Weight/max(G.Edges.Weight);
+
+% plot the graph
+h = plot(G,'EdgeLabel',G.Edges.Weight,'LineWidth',LWidths)
+bb = (aa'./sum(aa'))';
+nodecolor=bb(:,1:3);
+nodecolor(:,3) = bb(:,1);  % blue for math
+nodecolor(:,2) = bb(:,3);  % green for reading
+nodecolor(:,1) = bb(:,4);  % red for ClosedEye
+nodecolor = (nodecolor'./sum(bb'))';
+h.NodeColor = nodecolor;
+h.MarkerSize = sum(aa')./(max(max(aa))/20);
+h.NodeLabel = sum(aa')
+
+%}
+colors = ['b','r','g','y'];
+figure(3)
+
+%{
+for k = [1,2,3,4]
+    theColor = string('-*')+string(colors(k));
+    
+    plot3(score(CC==k,1),score(CC==k,2),score(CC==k,3),theColor); hold on;
+end
+%}
+k=1; plot3(score(CC==k,1),score(CC==k,2),score(CC==k,3),'-*b'); hold on;
+k=2; plot3(score(CC==k,1),score(CC==k,2),score(CC==k,3),'-*r'); hold on;
+k=3; plot3(score(CC==k,1),score(CC==k,2),score(CC==k,3),'-*g'); hold on;
+k=4; plot3(score(CC==k,1),score(CC==k,2),score(CC==k,3),'-*y'); hold on;
+hold off
+legend('math','openeye','reading','closedeye')
+title('PCA plots of four k-mean classifications')
+xlabel('pca1')
+ylabel('pca2')
+zlabel('pca3')
+grid on
+grid minor
+rotate3d on
+
+figure(6)
+
+indx = 1:length(C);
+indM = indx<3000;
+indC = indx>=3000& indx<6000;
+indR = indx>=6000 & indx<9000;
+indO = indx>=9000;
+ind=[indM;indC;indR;indO];
+plot3(score(indM,1),score(indM,2),score(indM,3),'-*b'); hold on;
+plot3(score(indC,1),score(indC,2),score(indC,3),'-*r'); hold on;
+plot3(score(indR,1),score(indR,2),score(indR,3),'-*g'); hold on;
+plot3(score(indO,1),score(indO,2),score(indO,3),'-*y'); hold on;
+hold off
+legend('math','openeye','reading','closedeye')
+title('PCA plots of four activities')
+xlabel('pca1')
+ylabel('pca2')
+zlabel('pca3')
+grid on
+grid minor
+rotate3d on
+
+
+figure(4)
+for k=[1:4]
+    subplot(3,2,k);scatter(score(CC==k,1),score(CC==k,2),colors(k)); 
+    grid on; grid minor; axis([-1,1,-1,1]);title(string('predicted')+string(k));
+end
+
+subplot(3,2,5);
+for k=[1:4]
+    scatter(score(CC==k,1),score(CC==k,2),colors(k)); hold on;
+    grid on; grid minor; axis([-1,1,-1,1]);
+end
+title('PCA 1,2 projection of Predicted Activities')
+subplot(3,2,6);
+for k=[1:4]
+    scatter(score(CC==k,1),score(CC==k,3),colors(k)); hold on;
+    grid on; grid minor; axis([-1,1,-1,1]);
+end
+title('PCA 1,3 projection of Predicted Activities')
+hold off
+
+figure(5)
+subplot(3,2,1)
+scatter(score(1:3000,1),score(1:3000,2),'b');
+axis([-1,1,-1,1]);title('Actual Math');grid on; grid minor;
+subplot(3,2,2)
+scatter(score(3000:6000,1),score(3000:6000,2),'r');
+axis([-1,1,-1,1]);title('Actual Open Eye Relaxation');grid on; grid minor;
+subplot(3,2,3)
+scatter(score(6000:9000,1),score(6000:9000,2),'g');
+axis([-1,1,-1,1]);title('Actual Reading');grid on; grid minor;
+subplot(3,2,4)
+scatter(score(9000:length(C),1),score(9000:length(C),2),'y');
+axis([-1,1,-1,1]);title('Actual ClosedEye Relaxation');grid on; grid minor;
+subplot(3,2,5)
+step=1;
+scatter(score(1:step:3000,1),score(1:step:3000,2),colors(1));hold on
+scatter(score(3000:step:6000,1),score(3000:step:6000,2),colors(2));
+scatter(score(6000:step:9000,1),score(6000:step:9000,2),colors(3));
+scatter(score(9000:step:length(C),1),score(9000:step:length(C),2),colors(4));hold off
+grid on; grid minor; axis([-1,1,-1,1]);
+title('PCA 1,2 projection of Predicted Activities');
+subplot(3,2,6)
+scatter(score(1:step:3000,1),score(1:step:3000,3),colors(1));hold on
+%scatter(score(3000:step:6000,1),score(3000:step:6000,3),colors(2));
+scatter(score(6000:step:9000,1),score(6000:step:9000,3),colors(3));
+%scatter(score(9000:step:length(C),1),score(9000:step:length(C),3),colors(4));hold off
+grid on; grid minor; axis([-1,1,-1,1]);
+title('PCA 1,3 projection of Predicted Activities')
+hold off
+
